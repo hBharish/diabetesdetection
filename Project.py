d01c0588a9a86c21429eb767f8a8d357e84bafa1
hBharish <97594646+hBharish@users.noreply.github.com>
2023-11-05 21:22:31 +0530

    Update Project.py

--- Project.py
+++ Project.py
@@ -28,7 +28,7 @@ def diabetes_prediction(input_data):
     input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
 
     prediction = loaded_model.predict(input_data_reshaped)
-    print(prediction)
+
 
     if (prediction[0] == 0):
       return 'The person is not diabetic'
